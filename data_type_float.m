f8f1a76d8608d74539e59757e54a68e391d81e8d
Tran Khanh <khanhth@evolableasia.vn>
2015-10-06 14:56:59 +0700

    Data types

---
+++ data_type_float.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+/* see more at: www.hoclaptrinh.org */
+int main()
+{
+	NSLog(@"Ví dụ về số float: %.6f \n", 0.123456);
+	return 0;
+}
---
+++ data_type_void.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+/* see more at: www.hoclaptrinh.org */
+void main()
+{
+   NSLog(@"Hàm main ở đây không trả về bất kỳ giá trị nào");
+}
