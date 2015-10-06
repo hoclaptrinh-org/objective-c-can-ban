eddac9026906814c6d361c3ffa8cc7ed8858b2c6
Tran Khanh <khanhth@evolableasia.vn>
2015-10-06 13:56:50 +0700

    NSLog

---
+++ NSLog_fn.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+ 
+/* Copyright: hoclaptrinh.org */
+ 
+int main ()
+{
+    /* Truyền nhiều hơn 1 đối số */
+    NSLog (@"Năm nay là năm: %i\n ", 2015);
+    return 0;
+}
